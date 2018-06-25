15dcca306e199317f7100bbd8db394119bff53c7
JaydenGarrick <33009669+JaydenGarrick@users.noreply.github.com>
2018-06-25 15:34:17 -0600

    Add anchor and rgb extension

---
+++ BadgingTracker/Resources/Extensions/UIColor+Extensions.swift
@@ -0,0 +1,15 @@
+//
+//  UIColor+Extensions.swift
+//  BadgingTracker
+//
+//  Created by Jayden Garrick on 6/25/18.
+//  Copyright © 2018 Nick Reichard. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    static func rgb(_ red: CGFloat, _ green: CGFloat, _ blue: CGFloat) -> UIColor {
+        return UIColor(red: red/255, green: green/255, blue: blue/255, alpha: 1)
+    }
+}
--- BadgingTracker/ViewControllers/FeedTab/FeedViewController.swift
+++ BadgingTracker/ViewControllers/FeedTab/FeedViewController.swift
@@ -13,6 +13,7 @@ class FeedViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .red
+
         // Do any additional setup after loading the view.
     }
 
